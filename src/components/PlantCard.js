6cbeabfddb07e7693b70020bfcdfca8f52f5af27
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-15 12:38:03 -0600

    lab complete

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, soldOut }) {
+  const [sold, setSold] = useState(plant.sold || false)
+
+  function handlesoldOut() {
+    soldOut(plant)
+    setSold(true)
+  }
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {!sold ? (<button className="primary" onClick={handlesoldOut}>In Stock</button>) : (<button>Out of Stock</button>)}
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, addPlant, soldOut }) {
+  const [searchPlant, setSearchPlant] = useState("")
+
+  const filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(searchPlant))
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm  addPlant={addPlant}/>
+      <Search setSearchPlant={setSearchPlant}/>
+      <PlantList plants={filteredPlants} soldOut={soldOut}/>
     </main>
   );
 }
